5745e312829247e3ca3d381cb971eb687cffd17e
Paweł Kędzia <pawel.kedzia@minebest.com>
2021-08-24 22:26:13 +0200

    Add prebuild

---
+++ src/bridges/index.ts
@@ -0,0 +1,2 @@
+export * from "./transfer-ERC20-from-ethereum-to-matic-using-pos-bridge";
+export * from "./transfer-ERC20-from-matic-to-ethereum-using-pos-bridge";
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {
+  transferERC20FromPolygonToEthereumUsingPOSBridge,
+  transferERC20FromEthereumToMaticUsingPOSBridge,
+} from "./bridges";
+
+export {
+  transferERC20FromPolygonToEthereumUsingPOSBridge,
+  transferERC20FromEthereumToMaticUsingPOSBridge,
+};
